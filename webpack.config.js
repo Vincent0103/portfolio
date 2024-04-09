2a37e4778727e54624662dcdae9fc7e4e3ebbee0
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-04-09 18:15:22 +0200

    Setup repo

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = dirname(__filename);
+
+export default {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/i,
+        use: 'raw-loader',
+      },
+    ],
+  },
+};
