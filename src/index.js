c870164cb1e0a72d8241cc0e259c7fd8fab133dc
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-04-21 22:22:17 +0200

    Add text invert effect and optimize rendering

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./style.css";
 
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  const main = document.body.querySelector("main");
+  const circleFillContainer = main.querySelector(".circle-fill-container");
+  const circleFill = circleFillContainer.querySelector(".circle-fill");
+
+  const updateHeight = (container) => {
+    const computedStyle = window.getComputedStyle(container);
+    circleFillContainer.style.height = `calc(${computedStyle.width} * (3 / 4))`;
+  }
+
+  updateHeight(circleFillContainer);
+  window.addEventListener('resize', () => updateHeight(circleFillContainer));
+});
