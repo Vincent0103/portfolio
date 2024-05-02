38bc6cd15180bc45b911840f58a379057dbd58bb
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-05-02 22:34:36 +0200

    Add caroussel script

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import "./style.css";
 window.addEventListener("DOMContentLoaded", () => {
   const imgSliderContainer = document.querySelector(".img-slider-container");
   const slidingImg = imgSliderContainer.querySelectorAll(".sliding-img");
-  imgSliderContainer.addEventListener('click', () => {
-    
+  imgSliderContainer.addEventListener("click", () => {
+    slidingImg.forEach((container) => {
+      if (container.classList.contains("carouselCenter")) {
+        container.classList.remove("carouselCenter");
+        container.classList.add("carouselLeft");
+      } else if (container.classList.contains("carouselLeft")) {
+        container.classList.remove("carouselLeft");
+        container.classList.add("carouselLeftLeft");
+      } else if (container.classList.contains("carouselLeftLeft")) {
+        container.classList.remove("carouselLeftLeft");
+        container.classList.add("carouselRightRight");
+      } else if (container.classList.contains("carouselRightRight")) {
+        container.classList.remove("carouselRightRight");
+        container.classList.add("carouselRight");
+      } else if (container.classList.contains("carouselRight")) {
+        container.classList.remove("carouselRight");
+        container.classList.add("carouselCenter");
+      }
+    });
   });
 });
