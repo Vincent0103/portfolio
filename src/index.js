7c62a61092818ce1d860b342615f30a16d4b8e05
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-05-02 20:55:03 +0200

    Change filter: brightness with opacity alternative for performance purposes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import "./style.css";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const main = document.body.querySelector("main");
-  const circleFillContainer = main.querySelector(".circle-fill-container");
-  const circleFill = circleFillContainer.querySelector(".circle-fill");
-
-  const updateHeight = (container) => {
-    const computedStyle = window.getComputedStyle(container);
-    circleFillContainer.style.height = `calc(${computedStyle.width} * (3 / 4))`;
-  }
-
-  updateHeight(circleFillContainer);
-  window.addEventListener('resize', () => updateHeight(circleFillContainer));
+  const imgSliderContainer = document.querySelector(".img-slider-container");
+  const slidingImg = imgSliderContainer.querySelectorAll(".sliding-img");
+  imgSliderContainer.addEventListener('click', () => {
+    
+  });
 });
