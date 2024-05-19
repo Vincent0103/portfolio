7598388466bba517bd18305482a5fdcd18d71a0b
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-05-19 22:06:32 +0200

    Add heading transition to projects title

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { ProjectDescription } from "./DOM.js";
 import Carousel from "./wrapper.js";
-import { throttle } from "./utils.js";
 
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -21,25 +20,35 @@ window.addEventListener("DOMContentLoaded", () => {
 
   carousel.handleCarouselClick(projectDescription);
 
-  const aboutSection = document.querySelector(".about-section");
-  const animatedH2Container = aboutSection.querySelector(".animated-h2-container");
-  const h2 = animatedH2Container.querySelector("h2");
-  const aboutTitleUnderline = animatedH2Container.querySelector(".underline");
-  const aboutTitleUnderlineShadow = animatedH2Container.querySelector(".underline-shadow");
+  const handleHeadingAnimations = (animatedHeadingContainer) => {
+    const h2Container = animatedHeadingContainer.querySelector(".h2-container");
+    const h2 = h2Container.querySelector("h2");
+    const aboutTitleUnderline = animatedHeadingContainer.querySelector(".underline");
+    const aboutTitleUnderlineShadow = animatedHeadingContainer.querySelector(".underline-shadow");
+    let timeoutId;
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        h2.classList.add("spawn-heading");
-        aboutTitleUnderline.classList.add("spawn-underline");
-        aboutTitleUnderlineShadow.classList.add("spawn-underline");
-      } else {
-        h2.classList.remove("spawn-heading");
-        aboutTitleUnderline.classList.remove("spawn-underline");
-        aboutTitleUnderlineShadow.classList.remove("spawn-underline");
-      }
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          h2.classList.add("spawn-heading");
+          aboutTitleUnderline.classList.add("spawn-underline");
+          aboutTitleUnderlineShadow.classList.add("spawn-underline");
+          timeoutId = setTimeout(() => {
+            h2Container.style.overflowY = "visible";
+          }, 800);
+        } else {
+          h2.classList.remove("spawn-heading");
+          aboutTitleUnderline.classList.remove("spawn-underline");
+          aboutTitleUnderlineShadow.classList.remove("spawn-underline");
+          clearTimeout(timeoutId);
+          h2Container.style.overflowY = "hidden";
+        }
+      });
     });
-  });
 
-  observer.observe(animatedH2Container);
+    observer.observe(animatedHeadingContainer);
+  };
+
+  const animatedH2Container = document.querySelectorAll(".animated-h2-container");
+  animatedH2Container.forEach((container) => { handleHeadingAnimations(container); });
 });
