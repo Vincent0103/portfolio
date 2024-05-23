49577711441e0a4961d6594987db8fbf82faee3e
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-05-23 10:37:33 +0200

    Fix tests not passing when classes where badly typed

--- src/tests/logic.test.js
+++ src/tests/logic.test.js
@@ -19,33 +19,33 @@ describe("initialize carousel project classes", () => {
   });
   test("initilialize correctly when there is 5 sliding images", () => {
     carouselLogic.initializeCarouselProjectsClasses(5);
-    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carouselLeftLeft", "carouselLeft", "carouselCenter", "carouselRight", "carouselRightRight"]);
+    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carousel-left-left", "carousel-left", "carousel-center", "carousel-right", "carousel-right-right"]);
   });
 
   test("initilialize correctly when there is 4 sliding images", () => {
     carouselLogic.initializeCarouselProjectsClasses(4);
-    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carouselLeftLeft", "carouselLeft", "carouselCenter", "carouselRight"]);
+    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carousel-left-left", "carousel-left", "carousel-center", "carousel-right"]);
   });
 
   test("initilialize correctly when there is 3 sliding images", () => {
     carouselLogic.initializeCarouselProjectsClasses(3);
-    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carouselLeft", "carouselCenter", "carouselRight"]);
+    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carousel-left", "carousel-center", "carousel-right"]);
   });
 
   test("initilialize correctly when there is 2 sliding images", () => {
     carouselLogic.initializeCarouselProjectsClasses(2);
-    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carouselLeft", "carouselCenter"]);
+    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carousel-left", "carousel-center"]);
   });
 
   test("initilialize correctly when there is 1 sliding images", () => {
     carouselLogic.initializeCarouselProjectsClasses(1);
-    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carouselCenter"]);
+    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carousel-center"]);
   });
 
   test("initilialize correctly when there is 8 sliding images", () => {
     carouselLogic.initializeCarouselProjectsClasses(8);
-    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carouselHideLeft", "carouselHideLeft",
-      "carouselLeftLeft", "carouselLeft", "carouselCenter", "carouselRight", "carouselRightRight", "carouselHideRight"]);
+    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carousel-hide-left", "carousel-hide-left",
+      "carousel-left-left", "carousel-left", "carousel-center", "carousel-right", "carousel-right-right", "carousel-hide-right"]);
   });
 });
 
@@ -59,12 +59,12 @@ describe("move carousel project classes", () => {
   test("shift carousel classes 2 times on the left", () => {
     carouselLogic.moveCarouselProjectsClasses(true);
     carouselLogic.moveCarouselProjectsClasses(true);
-    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carouselCenter", "carouselRight", "carouselLeftLeft", "carouselLeft"]);
+    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carousel-center", "carousel-right", "carousel-left-left", "carousel-left"]);
 
     carouselLogic2.moveCarouselProjectsClasses(true);
     carouselLogic2.moveCarouselProjectsClasses(true);
-    expect(carouselLogic2.getProjectsCarouselClasses()).toEqual(["carouselRightRight", "carouselHideRight",
-      "carouselHideLeft", "carouselLeftLeft", "carouselLeft", "carouselCenter", "carouselRight",
+    expect(carouselLogic2.getProjectsCarouselClasses()).toEqual(["carousel-right-right", "carousel-hide-right",
+      "carousel-hide-left", "carousel-left-left", "carousel-left", "carousel-center", "carousel-right",
     ]);
   });
 
@@ -72,12 +72,12 @@ describe("move carousel project classes", () => {
     carouselLogic.moveCarouselProjectsClasses(true);
     carouselLogic.moveCarouselProjectsClasses(false);
     carouselLogic.moveCarouselProjectsClasses(true);
-    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carouselRight", "carouselLeftLeft", "carouselLeft", "carouselCenter"]);
+    expect(carouselLogic.getProjectsCarouselClasses()).toEqual(["carousel-right", "carousel-left-left", "carousel-left", "carousel-center"]);
 
     carouselLogic2.moveCarouselProjectsClasses(false);
     carouselLogic2.moveCarouselProjectsClasses(true);
-    expect(carouselLogic2.getProjectsCarouselClasses()).toEqual(["carouselHideLeft", "carouselLeftLeft",
-      "carouselLeft", "carouselCenter", "carouselRight", "carouselRightRight", "carouselHideRight"]);
+    expect(carouselLogic2.getProjectsCarouselClasses()).toEqual(["carousel-hide-left", "carousel-left-left",
+      "carousel-left", "carousel-center", "carousel-right", "carousel-right-right", "carousel-hide-right"]);
   });
 
   test("carousel can get back to it's original state after going around n times", () => {
@@ -85,10 +85,10 @@ describe("move carousel project classes", () => {
     carouselLogic3.moveCarouselProjectsClasses(false);
     carouselLogic3.moveCarouselProjectsClasses(false);
     carouselLogic3.moveCarouselProjectsClasses(false);
-    expect(carouselLogic3.getProjectsCarouselClasses()).toEqual(["carouselLeft", "carouselCenter"]);
+    expect(carouselLogic3.getProjectsCarouselClasses()).toEqual(["carousel-left", "carousel-center"]);
 
     carouselLogic3.moveCarouselProjectsClasses(true);
     carouselLogic3.moveCarouselProjectsClasses(true);
-    expect(carouselLogic3.getProjectsCarouselClasses()).toEqual(["carouselLeft", "carouselCenter"]);
+    expect(carouselLogic3.getProjectsCarouselClasses()).toEqual(["carousel-left", "carousel-center"]);
   });
 });
